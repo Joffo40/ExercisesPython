056a5a4192f02b1964b54c473c3fc17aa68e3327
Joffo40 <142779100+Joffo40@users.noreply.github.com>
2025-11-12 16:02:30 +0100

    Exercise2

---
+++ Exercise2.py
@@ -0,0 +1,12 @@
+nom = "Dupont"
+
+age = 30
+
+taille=1.60
+
+print(f"Bonjour, je m'appelle {nom} et j'ai {age} ans et la taille de {taille} m")
+print(f"Les types sont les suivants:")
+print(type(nom))
+print(type(age))
+print(type(taille))
+
