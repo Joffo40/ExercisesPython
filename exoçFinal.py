6511f179bfd4a1922f829411ce4997721282a637
Joffo40 <142779100+Joffo40@users.noreply.github.com>
2025-12-04 18:05:27 +0100

    Evolution extraction HTML

--- exoçFinal.py
+++ exoçFinal.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-
+import re
 
 
 with open("index.html", 'r') as file:
@@ -11,26 +11,19 @@ with open("index.html", 'r') as file:
     #print(listeProduits)
 
 
-    listeProduit=soup.find_all('h2')
+    #listeProduit=soup.find_all('h2')
 
     listePrix=soup.find_all("p",class_ = "price")
 
 
-    print(listeProduits)
-    print(listePrix)
-    produit= dict()
+    #print(listeProduits)
+    #print(listePrix)
     #listeProduits[0]=listeProduits[0].replace('<h2>','')
-    
-   # listePrix[0]=listePrix[0].replace('Prix: ',' ')
+    #listePrix[0]=listePrix[0].replace('Prix: ',' ')
     #listePrix[1]=listePrix[1].replace('Prix: ',' ')
     #listePrix[2]=listePrix[2].replace('Prix: ',' ')
-    
-    
-    
-print(listeProduits[0])    
-print(listePrix[0].replace("Prix: ","price"))
-print(listeProduits[1]) 
-print(listePrix[1].replace('Prix: ',' price'))
-print(listeProduits[2]) 
-print(listePrix[2].replace('Prix: ',' price'))
-print (listePrix)
+
+    produits= {listeProduits[0]:listePrix[0],listeProduits[1]:listePrix[1],listeProduits[2]:listePrix[2]}
+    #test= listeProduits.split("<h2>")
+    print(listeProduits[0].string)
+    print((listePrix[0]).string.split('Prix:','€'))
