69aee6febc9883fda2d23b3c09e8fb780bc60b86
Joffo40 <142779100+Joffo40@users.noreply.github.com>
2025-12-17 21:27:46 +0100

    Premier exercise Classes et Objets

---
+++ Exercise10.py
@@ -0,0 +1,29 @@
+class ToolBox:
+    def __init__(self, tools):
+        self.tools=[]
+
+    def add_tool(self,tools):
+
+        pass
+    def remove_tool(self,tools):
+        pass
+class ScrewDriver:
+    def __init__(self, size):
+        self.size=size
+
+    def tighten(self,screw):
+
+        pass
+    def loosen(self,screw):
+        pass
+
+class Hammer:
+    def __init__(self, color='Red'):
+        self.color=color
+
+    def hammer_in(self,nail):
+
+        pass
+    
+    def remove(self, nail):
+        pass
